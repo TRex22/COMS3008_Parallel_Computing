bc1d384d485a65d0b35dcd50a4630827e20c3afb
TRex22 <contact@jasonchalom.com>
2016-07-25 11:46:17 +0200

    Again

--- lab1/kickoff.sh
+++ lab1/kickoff.sh
@@ -6,7 +6,7 @@
 
 #Example 1 data process
 ./dataProcessor.out results/Example1_col.csv results/mine/Example1_col_avg.csv 50
-./dataProcessor.out results/Example1_row.csv results/mine/Example1_col_avg.csv 50
+./dataProcessor.out results/Example1_row.csv results/mine/Example1_row_avg.csv 50
 
 #Example 2 data process
 ./dataProcessor.out results/Example3_1_VaryNoThreads_FixedArraySize.csv results/mine/Example3_1_VaryNoThreads_FixedArraySize_avg.csv 50
