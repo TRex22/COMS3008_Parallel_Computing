88587e8f1033e3d84d8df5bf089b3d2f84b6d1ae
trex22 <trex22@jasonchalom.com>
2016-08-04 21:42:10 +0200

    mores reduction

--- lab2/kickoff.sh
+++ lab2/kickoff.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #n averageNumber incrementSize noIncrements writeFile? noThreads
 mkdir results
-./example1.out 1000 50 3000 20000 1 4
-./example2.out 1000 50 3000 20000 1 4
-./example3.out 1000 50 3000 20000 1 4
-./example4.out 1000 50 3000 20000 1 4
+./example1.out 1000 25 3000 1000 1 4
+./example2.out 1000 25 3000 1000 1 4
+./example3.out 1000 25 3000 1000 1 4
+./example4.out 1000 25 3000 1000 1 4
 
 #./dataProcessor.out
 mkdir results/mine
-./dataProcessor.out results/Example1.csv results/mine/Example1.csv 50
-./dataProcessor.out results/Example2.csv results/mine/Example2.csv 50
-./dataProcessor.out results/Example3.csv results/mine/Example3.csv 50
-./dataProcessor.out results/Example4.csv results/mine/Example4.csv 50
+./dataProcessor.out results/Example1.csv results/mine/Example1.csv 25
+./dataProcessor.out results/Example2.csv results/mine/Example2.csv 25
+./dataProcessor.out results/Example3.csv results/mine/Example3.csv 25
+./dataProcessor.out results/Example4.csv results/mine/Example4.csv 25
